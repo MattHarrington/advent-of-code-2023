730a31a3c63e316dd4db64830595e2e614fd2bb1
Matt Harrington <mattharrington@users.noreply.github.com>
2023-12-05 22:55:32 -0800

    Faster solution for day 1

--- day01/src/libaoc.cpp
+++ day01/src/libaoc.cpp
@@ -2,7 +2,6 @@
 #include <cctype>
 #include <fstream>
 #include <regex>
-#include <sstream>
 #include <stdexcept>
 
 #include "libaoc.hpp"
@@ -10,12 +9,16 @@
 std::vector<std::string> read_input(const std::string& filename)
 {
     std::fstream in{ filename };
-    if (!in) throw std::runtime_error("File not found");
+    if (!in)
+    {
+        throw std::runtime_error("File not found");
+    }
 
     std::vector<std::string> calibration_values;
     std::string line;
 
-    while (getline(in, line)) {
+    while (getline(in, line))
+    {
         calibration_values.push_back(line);
     }
 
@@ -25,20 +28,19 @@ std::vector<std::string> read_input(const std::string& filename)
 int part1(const std::vector<std::string>& calibration_values)
 {
     int sum{ 0 };
-    for (const auto& calibration_value : calibration_values)
+    for (const auto& line : calibration_values)
     {
         // Search forwards for digit
-        std::stringstream numeric_calibration_value_ss;
-        auto first_digit_it = std::find_if(std::cbegin(calibration_value), std::cend(calibration_value),
+        auto first_digit_it = std::find_if(std::cbegin(line), std::cend(line),
             [](const auto& c) { return std::isdigit(c); });
-        numeric_calibration_value_ss << *first_digit_it;
+        int first_digit{ *first_digit_it - '0' };
 
         // Search backwards for digit
-        auto last_digit_it = std::find_if(std::crbegin(calibration_value), std::crend(calibration_value),
+        auto last_digit_it = std::find_if(std::crbegin(line), std::crend(line),
             [](const auto& c) { return std::isdigit(c); });
-        numeric_calibration_value_ss << *last_digit_it;
+        int second_digit{ *last_digit_it - '0' };
 
-        sum += std::stoi(numeric_calibration_value_ss.str());
+        sum += (10 * first_digit + second_digit);
     }
 
     return sum;
@@ -47,109 +49,112 @@ int part1(const std::vector<std::string>& calibration_values)
 int part2(const std::vector<std::string>& calibration_values)
 {
     int sum{ 0 };
-    const auto r{ std::regex(R"((one|two|three|four|five|six|seven|eight|nine|\d))") };
+    const auto r_forwards{ std::regex(R"((one|two|three|four|five|six|seven|eight|nine|\d))") };
+    const auto r_backwards{ std::regex(R"((eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|\d))") };
 
-    for (const auto& calibration_value_s : calibration_values)
+    for (const auto& line : calibration_values)
     {
-        std::smatch sm;
-        std::vector<std::string> matches;
+        std::smatch m_fwd;
+        std::match_results<std::string::const_reverse_iterator> m_rev;
+        std::string first_match;
+        std::string last_match;
 
-        auto start_pos{ std::cbegin(calibration_value_s) };
-        while (std::regex_search(start_pos, std::cend(calibration_value_s), sm, r))
+        if (std::regex_search(std::cbegin(line), std::cend(line), m_fwd, r_forwards))
         {
-            matches.emplace_back(sm[0]);
-            //start_pos = sm.suffix().first; // fails on "9onekptdkglrnloneightfr"
-            ++start_pos;
+            first_match = m_fwd[0];
+        }
+        if (std::regex_search(std::crbegin(line), std::crend(line), m_rev, r_backwards))
+        {
+            last_match = m_rev[0];
         }
 
         int first_digit{ 0 };
         int second_digit{ 0 };
 
-        if (matches.front() == "one" || matches.front() == "1")
+        if (first_match == "one" || first_match == "1")
         {
             first_digit = 1;
         }
-        else if (matches.front() == "two" || matches.front() == "2")
+        else if (first_match == "two" || first_match == "2")
         {
             first_digit = 2;
         }
-        else if (matches.front() == "three" || matches.front() == "3")
+        else if (first_match == "three" || first_match == "3")
         {
             first_digit = 3;
         }
-        else if (matches.front() == "four" || matches.front() == "4")
+        else if (first_match == "four" || first_match == "4")
         {
             first_digit = 4;
         }
-        else if (matches.front() == "five" || matches.front() == "5")
+        else if (first_match == "five" || first_match == "5")
         {
             first_digit = 5;
         }
-        else if (matches.front() == "six" || matches.front() == "6")
+        else if (first_match == "six" || first_match == "6")
         {
             first_digit = 6;
         }
-        else if (matches.front() == "seven" || matches.front() == "7")
+        else if (first_match == "seven" || first_match == "7")
         {
             first_digit = 7;
         }
-        else if (matches.front() == "eight" || matches.front() == "8")
+        else if (first_match == "eight" || first_match == "8")
         {
             first_digit = 8;
         }
-        else if (matches.front() == "nine" || matches.front() == "9")
+        else if (first_match == "nine" || first_match == "9")
         {
             first_digit = 9;
         }
         else
         {
-            throw std::domain_error("No matched first digit");
+            throw std::invalid_argument("No matched first digit");
         }
 
-        if (matches.back() == "one" || matches.back() == "1")
+        if (last_match == "eno" || last_match == "1")
         {
             second_digit = 1;
         }
-        else if (matches.back() == "two" || matches.back() == "2")
+        else if (last_match == "owt" || last_match == "2")
         {
             second_digit = 2;
         }
-        else if (matches.back() == "three" || matches.back() == "3")
+        else if (last_match == "eerht" || last_match == "3")
         {
             second_digit = 3;
         }
-        else if (matches.back() == "four" || matches.back() == "4")
+        else if (last_match == "ruof" || last_match == "4")
         {
             second_digit = 4;
         }
-        else if (matches.back() == "five" || matches.back() == "5")
+        else if (last_match == "evif" || last_match == "5")
         {
             second_digit = 5;
         }
-        else if (matches.back() == "six" || matches.back() == "6")
+        else if (last_match == "xis" || last_match == "6")
         {
             second_digit = 6;
         }
-        else if (matches.back() == "seven" || matches.back() == "7")
+        else if (last_match == "neves" || last_match == "7")
         {
             second_digit = 7;
         }
-        else if (matches.back() == "eight" || matches.back() == "8")
+        else if (last_match == "thgie" || last_match == "8")
         {
             second_digit = 8;
         }
-        else if (matches.back() == "nine" || matches.back() == "9")
+        else if (last_match == "enin" || last_match == "9")
         {
             second_digit = 9;
         }
         else
         {
-            throw std::domain_error("No matched second digit");
+            throw std::invalid_argument("No matched second digit");
         }
 
-        int calibration_value_i{ 10 * first_digit + second_digit };
-        //std::cout << calibration_value_s << "\t" << calibration_value_i << "\n";
-        sum += calibration_value_i;
+        int calibration_value{ 10 * first_digit + second_digit };
+        sum += calibration_value;
     }
 
     return sum;
