8e2072aeb04efc6f9c3b22dec292773e396c1fa7
Matt Harrington <mattharrington@users.noreply.github.com>
2023-12-08 11:42:30 -0800

    Day 3 part 1 refactored

--- day03/libday03.cpp
+++ day03/libday03.cpp
@@ -36,42 +36,46 @@ int part1(const std::vector<std::string>& schematic)
         for (auto i{std::sregex_iterator(std::cbegin(line), std::cend(line), r)}; i != std::sregex_iterator(); ++i)
         {
             auto match{ *i };
-            auto pos{ match.position()};
+            auto match_pos{ match.position()};
             auto match_length{ match.length(1) };
+            bool part_found_row_above{ false };
 
             // Check left
-            if (pos > 0 && line.at(pos - 1) != '.' )
+            if (match_pos > 0 && line.at(match_pos - 1) != '.' )
             {
                 part_numbers.push_back(std::stoi(match.str()));
+                continue;
             }
 
             // Check right
-            else if (static_cast<size_t>(pos) < line.size() - match_length && (line.at(pos + match_length) != '.'))
+            else if (static_cast<size_t>(match_pos) < line.size() - match_length && (line.at(match_pos + match_length) != '.'))
             {
                 part_numbers.push_back(std::stoi(match.str()));
+                continue;
             }
 
             // Check row above
-            else if (row > 0)
+            if (row > 0)
             {
                 for (auto i{ 0 }; i < match_length + 2; ++i)
                 {
                     auto prev_row{ schematic.at(row - 1) };
-                    if (pos + i > 0 && pos + i < line.size() && prev_row.at(pos + i - 1) != '.' )
+                    if (match_pos + i > 0 && match_pos + i < line.size() && prev_row.at(match_pos + i - 1) != '.' )
                     {
                         part_numbers.push_back(std::stoi(match.str()));
+                        part_found_row_above = true; // Prevents checking row below
                         break;
                     }
                 }
             }
 
             // Check row below
-            if (row < schematic.size() - 1)
+            if (!part_found_row_above && row < schematic.size() - 1)
             {
                 auto next_row{ schematic.at(row + 1) };
                 for (auto i{ 0 }; i < match_length + 2; ++i)
                 {
-                    if (pos + i > 0 && pos + i < line.size() && next_row.at(pos + i - 1) != '.' )
+                    if (match_pos + i > 0 && match_pos + i < line.size() && next_row.at(match_pos + i - 1) != '.' )
                     {
                         part_numbers.push_back(std::stoi(match.str()));
                         break;
